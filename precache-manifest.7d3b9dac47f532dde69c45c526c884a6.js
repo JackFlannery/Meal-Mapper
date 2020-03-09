c20053c1b0566cb3ae7081756da01dff108e6291
JackFlannery <jackryanflannery@gmail.com>
2020-03-08 23:30:01 -0400

    Updates

---
+++ precache-manifest.7d3b9dac47f532dde69c45c526c884a6.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b0f3b06068571b96b61fa968ca27a23f",
+    "url": "/Meal-Mapper/index.html"
+  },
+  {
+    "revision": "5fd4307e42284cfd04c5",
+    "url": "/Meal-Mapper/static/css/main.74eb9d1a.chunk.css"
+  },
+  {
+    "revision": "e5b3ca0d1944d1e9f9fa",
+    "url": "/Meal-Mapper/static/js/2.f9c7ca5b.chunk.js"
+  },
+  {
+    "revision": "577ef60f73636c6e95b316a24bc4f83f",
+    "url": "/Meal-Mapper/static/js/2.f9c7ca5b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5fd4307e42284cfd04c5",
+    "url": "/Meal-Mapper/static/js/main.75c87f1e.chunk.js"
+  },
+  {
+    "revision": "e7cf9fb12c8babeab64d",
+    "url": "/Meal-Mapper/static/js/runtime-main.11e68a31.js"
+  },
+  {
+    "revision": "12afd97937e1c75406627c3a9d9d8da9",
+    "url": "/Meal-Mapper/static/media/mapbox-icon.12afd979.png"
+  },
+  {
+    "revision": "b90c84633325fbfc6ed110ab459216bd",
+    "url": "/Meal-Mapper/static/media/star.b90c8463.png"
+  }
+]);
